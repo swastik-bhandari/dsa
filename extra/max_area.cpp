074790243be4d80fe08e4a184e69c809041fabbb
User <parrot@localhost.localdomain>
2025-02-14 04:08:07 +0000

    max_area problem

---
+++ extra/max_area.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int max_area(vector<vector<bool>>& v) {
+  int row_size = v[0].size();
+int col_size = v.size();
+int found_max_width = 0;
+int check_max_width =1;
+int counter =0;
+vector<int>freq(row_size, 0);
+
+for(int i = 0 ; i<col_size ; i++) {
+counter = 0;
+for(int j=0 ; j<row_size; j++) {
+if(v[i][j]) {
+freq[j]++;
+if(check_max_width <= freq[j]) {
+counter++;
+if(check_max_width == counter) {
+found_max_width++;
+check_max_width++;
+counter=0;
+}
+continue;
+}
+} ///////////////// 1
+else {
+freq[j]=0;
+} /////////////// 0
+counter=0;
+}
+}
+return found_max_width*found_max_width;
+}
+
+int main() {
+    vector<vector<bool>> v = {
+        {1, 0, 0, 1, 1, 0, 1, 1},
+        {1, 0, 0, 0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0, 0, 1, 1},
+        {0, 0, 0, 1, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 1, 1, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 1, 0, 1, 1, 0},
+        {0, 1, 1, 0, 1, 1, 1, 0}
+    };
+
+    cout << max_area(v);
+    return 0;
+}
