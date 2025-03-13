9c0d45281cedc3f5311404377f11fdfdfb450042
User <parrot@localhost.localdomain>
2025-03-13 15:33:44 +0000

    compared my freqency based approach with standard DP approach to solve maximal square problem using chrono

--- extra/max_area.cpp
+++ extra/max_area.cpp
@@ -1,3 +1,6 @@
+
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -7,19 +10,21 @@ int max_area(vector<vector<bool>>& v) {
 int col_size = v.size();
 int found_max_width = 0;
 int check_max_width =1;
+int check_max_height=1;
 int counter =0;
 vector<int>freq(row_size, 0);
 
 for(int i = 0 ; i<col_size ; i++) {
-counter = 0;
+
 for(int j=0 ; j<row_size; j++) {
 if(v[i][j]) {
 freq[j]++;
-if(check_max_width <= freq[j]) {
+if(check_max_height <= freq[j]) {
 counter++;
 if(check_max_width == counter) {
 found_max_width++;
 check_max_width++;
+check_max_height++;
 counter=0;
 }
 continue;
@@ -30,20 +35,18 @@ freq[j]=0;
 } /////////////// 0
 counter=0;
 }
+counter = 0; /////// 
 }
 return found_max_width*found_max_width;
 }
 
 int main() {
     vector<vector<bool>> v = {
-        {1, 0, 0, 1, 1, 0, 1, 1},
-        {1, 0, 0, 0, 0, 1, 0, 0},
-        {0, 1, 1, 1, 0, 0, 1, 1},
-        {0, 0, 0, 1, 0, 0, 0, 1},
-        {0, 0, 0, 0, 0, 1, 1, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 0, 0, 1, 0, 1, 1, 0},
-        {0, 1, 1, 0, 1, 1, 1, 0}
+        {1,1,0,1},
+        {1,1,0,1},
+        {1,1,1,1},
+    
+        
     };
 
     cout << max_area(v);
