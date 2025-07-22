b55da8cd7357f497254b66f16c1439573cd2c59e
swastik-bhandari <swastikbhandari2006@gmail.com>
2025-07-22 22:10:43 +0545

    Bitwise operators AND OR XOR NOT(one's complement) . NOT only flips the bit. But it is treated as 2's complement internally by the system

---
+++ bitmanipulation/bitwiseOperator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  /////// and operator & //////////
+  cout<< (3 & 5);   // 011 & 101 = 001  (1)
+
+  ////// OR operator | ////////////
+  cout<< (3 | 5); /// 011 | 101 = 111   (7)
+
+  /////// XOR operator ^ ////////////
+  cout<< (3 ^ 5);//// 011 ^ 101 = 110   (6)
+
+  cout<< (~2); // 3
+
+  ////////////// ~ => one's complement . for 4 bit system , it  is stored as 1101 . first bit is sign 1= neg , 101 (5) is magnitude. 1101 is stored in system . But while printing output . While printing that negative number , The internal system using 2's complement . 
+
+  /// for 1's complement . We put the symbol according to MSB (+ if 0 , - if 1) . we take 1's complement of the number (~101(5) => 010(2)) . 
+
+  /// for 2's complement . We put the symbol according to MSB (+ if 0 , - if 1) . we take 2's complement of the number . 1's complement of (5) + 1 => 010+1 => 011 (3) . 
+  return 0;
+}
